944db37707ef553acb9950fb7b1829f4f033dfe6
jeanzhou31 <jeanzhou31@gmail.com>
2016-07-28 15:05:19 -0400

    finished google part

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import FacebookLogin from 'react-facebook-login';
+import GoogleLogin from 'react-google-login';
 
 // example class based component (smart component)
 class App extends Component {
@@ -7,10 +8,15 @@ class App extends Component {
     super(props);
 
     this.state = {
-
+      google: false,
+      googleName: '',
+      googleEmail: '',
+      googleImage: '',
     };
 
     this.onFacebookLogin = this.onFacebookLogin.bind(this);
+    this.onGoogleLogin = this.onGoogleLogin.bind(this);
+    this.renderGoogle = this.renderGoogle.bind(this);
   }
 
 
@@ -24,6 +30,43 @@ class App extends Component {
     }
   }
 
+    // What happens after someone logs in
+  onGoogleLogin(response) {
+    if (response) {
+      const user = response.getBasicProfile();
+      this.setState({
+        google: true,
+        googleName: user.getName(),
+        googleEmail: user.getEmail(),
+        googleImage: user.getImageUrl(),
+      });
+    }
+  }
+
+  renderGoogle() {
+    if (this.state.google) {
+      return (
+        <div>
+          <ul>
+            <li>Thanks for logging in! </li>
+            <li>Your Name: {this.state.googleName}</li>
+            <li>Your Email: {this.state.googleEmail}</li>
+            <li>Your Photo: <img src={this.state.googleImage} alt="N/A" /></li>
+          </ul>
+        </div>
+     );
+    } else {
+      return (
+        <GoogleLogin
+          clientId="571169014678-lhplknaav70ufllj5fssmkq6jbf8s45c.apps.googleusercontent.com"
+          buttonText="LOGIN WITH GOOGLE"
+          callback={this.onGoogleLogin}
+        />
+      );
+    }
+  }
+
+
   render() {
     // Add in FacebookLogin, GoogleLogin and FirebaseApp
     return (
@@ -35,6 +78,9 @@ class App extends Component {
             fields="name,email,picture.type(large)"
             callback={this.onFacebookLogin}
           />
+        </div>
+        <div id="google">
+          {this.renderGoogle()}
         </div>
       </div>
     );
